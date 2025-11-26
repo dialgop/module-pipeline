60b02c2cefd0f592d169e411afd7c722a80e924a
dialgop <dialgop@gmail.com>
2025-11-26 23:20:39 +0100

    Create a vector like a pipeline of sensors and processor

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
+#include <memory>
+#include <vector>
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include "CameraSensor.h"
+#include "MovementDetector.h"
+#include  "ColorProcessor.h"
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    std::vector <std::shared_ptr<IModule>> pipeline;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    pipeline.push_back(std::make_shared<CameraSensor>(1));
+    pipeline.push_back(std::make_shared<MovementDetector>());
+    pipeline.push_back(std::make_shared<ColorProcessor>());
+
+    for (auto& pipe : pipeline) {
+        std::cout << "== Executing: " << pipe->name() << " ==" << std::endl;
+        pipe->run();
+        std::cout << std::endl;
     }
-
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }

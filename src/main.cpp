75eba37a8c46aa786d4c0f062ecec85b3ae4d10d
dialgop <dialgop@gmail.com>
2025-11-30 22:18:34 +0100

    Add styling

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,22 @@
 int main() {
     std::vector <std::shared_ptr<IModule>> pipeline;
 
+    int counter = 0;
+
     pipeline.push_back(std::make_shared<CameraSensor>(1));
     pipeline.push_back(std::make_shared<MovementDetector>());
     pipeline.push_back(std::make_shared<ColorProcessor>());
 
-    std::cout << "Executing: \n";
-    std::for_each(pipeline.begin(), pipeline.end(), [](const std::shared_ptr<IModule>& module){std::cout << module->name() << "\n";});
+    std::cout << "Executing pipeline: \n";
+
+    std::for_each(pipeline.begin(), pipeline.end(),
+        [&counter](const std::shared_ptr<IModule>& module) {
+            std::cout << module->name() << "\n";
+            module->run();
+            counter++;
+        });
+
+    std::cout << "\nTotal executed modules: " << counter << std::endl;
 
     return 0;
 }

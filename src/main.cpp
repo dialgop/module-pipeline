26fe300d78746cf7184377bb7688a073ead565b4
dialgop <dialgop@gmail.com>
2026-01-21 00:29:28 +0100

    Add should_run lambda function and changes modules counting

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,37 @@
 int main() {
     std::vector <std::shared_ptr<IModule>> pipeline;
 
-    int counter = 0;
-
     pipeline.push_back(std::make_shared<CameraSensor>(1));
     pipeline.push_back(std::make_shared<MovementDetector>());
     pipeline.push_back(std::make_shared<ColorProcessor>());
 
+    auto should_run = [](const std::shared_ptr<IModule>& m) {
+        return m->name().find("Detector") != std::string::npos;
+    };
+
+    auto make_counter = []() {
+        int c=0;
+        return [c]() mutable {return ++c;};
+    };
+
+    auto counter = make_counter();
+    int executed = 0;
+
     std::cout << "Executing pipeline: \n";
 
     std::for_each(pipeline.begin(), pipeline.end(),
-        [&counter](const std::shared_ptr<IModule>& module) {
-            std::cout << module->name() << "\n";
-            module->run();
-            counter++;
-        });
+    [&](const std::shared_ptr<IModule>& m) {
+        if (should_run(m)) {
+            std::cout << "RUN: " << m->name() << "\n";
+            m->run();
+            executed = counter();
+        } else {
+            std::cout << "SKIP: " << m->name() << "\n";
+        }
+    }
+);
 
-    std::cout << "\nTotal executed modules: " << counter << std::endl;
+    std::cout << "\nTotal executed modules: " << executed << std::endl;
 
     return 0;
 }
